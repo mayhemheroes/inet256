78ddbaccb2da00f7385b5f16bdac8ecb12c6890a
Brandon Watson <bmwtsn99@gmail.com>
2023-04-16 20:59:02 -0700

    add GitHub action to launch Mayhem

---
+++ mayhem/fuzzInet256.go
@@ -0,0 +1,74 @@
+package fuzzInet256
+
+import (
+    "strconv"
+    fuzz "github.com/AdaLogics/go-fuzz-headers"
+
+    "github.com/inet256/inet256/client/go_client/inet256client"
+    "github.com/inet256/inet256/pkg/bitstr"
+)
+
+func mayhemit(bytes []byte) int {
+
+    var num int
+    if len(bytes) > 10 {
+        num, _ = strconv.Atoi(string(bytes[0]))
+        bytes = bytes[1:]
+
+        switch num {
+
+        case 0:
+            fuzzConsumer := fuzz.NewConsumer(bytes)
+            var testStruct bitstr.BytesLSB
+            var testInt int
+
+            testStruct.Bytes = bytes
+            testStruct.Begin = 0
+            testStruct.End = len(bytes)
+
+            err := fuzzConsumer.CreateSlice(&testInt)
+            if err != nil {
+                return 0
+            }
+
+            testStruct.At(testInt)
+
+            return 0
+
+        case 1:
+            fuzzConsumer := fuzz.NewConsumer(bytes)
+            var testStruct bitstr.BytesLSB
+            var testInt int
+
+            testStruct.Bytes = bytes
+            testStruct.Begin = 0
+            testStruct.End = len(bytes)
+
+            err := fuzzConsumer.CreateSlice(&testInt)
+            if err != nil {
+                return 0
+            }
+
+            testStruct.Len()
+
+            return 0
+    
+        default:
+            fuzzConsumer := fuzz.NewConsumer(bytes)
+            var fuzzString string
+            err := fuzzConsumer.CreateSlice(&fuzzString)
+            if err != nil {
+                return 0
+            }
+
+            inet256client.NewClient(fuzzString)
+            return 0
+        }
+    }
+    return 0
+}
+
+func Fuzz(data []byte) int {
+    _ = mayhemit(data)
+    return 0
+}
